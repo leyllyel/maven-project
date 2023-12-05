fb59eb3061a16eb36b5ff0ecee4039c4c72f4094
leyl <sad.ley@icloud.com>
2023-12-05 19:11:55 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBookTest.java
+++ src/main/java/org/example/PhoneBookTest.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int count = phoneBook.add("John", "123456789");
+        assertEquals(1, count);
+    }
 }
